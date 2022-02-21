e770689d1bd6fbd452334dffaeb2ea81fb7ddf98
manjos <manjos@unica-tecnologia.com.br>
2022-02-20 22:26:45 -0300

    adição Identity

--- ControleFinanceiro.BLL/Models/Funcao.cs
+++ ControleFinanceiro.BLL/Models/Funcao.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ControleFinanceiro.BLL.Models
 {
-    class Funcao
+    class Funcao : IdentityRole<string>
     {
     }
 }
--- ControleFinanceiro.BLL/Models/Usuario.cs
+++ ControleFinanceiro.BLL/Models/Usuario.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ControleFinanceiro.BLL.Models
 {
-    class Usuario
+    class Usuario : IdentityUser<string>
     {
+        public int CPF { get; set; }
+
+        public string Profissao { get; set; }
+
+        public byte[] foto { get; set; }
+
+        public virtual ICollection<Cartao> Cartoes { get; set; }
+
+        public virtual ICollection<Ganho> Ganhos { get; set; }
+
+        public virtual ICollection<Despesa> Despesas { get; set; }
     }
 }
