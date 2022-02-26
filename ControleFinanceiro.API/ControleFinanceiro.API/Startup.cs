bf816e307520882ed965949fc59cb569f302fe57
manjos <manjos@unica-tecnologia.com.br>
2022-02-25 21:10:29 -0300

    criacao db

--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -1,7 +1,10 @@
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,10 @@ namespace ControleFinanceiro.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
+
+            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+
             services.AddCors();
 
             //configurando diretorio com arquivos do angular
